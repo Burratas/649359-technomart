dbba36adaee86b9c41c493a0bd1eeb87744c5c34
Burratas <v.b.irina@yandex.ru>
2018-09-11 10:44:42 +0500

    Проверка

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
-var link = document.querySelector(".button-link");
-var popup = document.querySelector(".modal-write-us");
-var close = document.querySelector(".modal-close");
-var overlay = document.querySelector(".overlay");
+var overlay = document.querySelector('.overlay');
 
-var link = document.querySelector(".buy");
-var popup = document.querySelector(".modal-cart");
+if(document.querySelector('.modal-cart')){
+  var modalCart = document.querySelector('.modal-cart');
+  var buttonCloseCart = modalCart.querySelector('.modal-close');
+  var buttonCart = document.querySelector('.buy');
 
+  buttonCart.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.add('is-show');
+    modalCart.classList.add('is-show');
+  });
 
+  buttonCloseCart.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.remove('is-show');
+    modalCart.classList.remove('is-show');
+  });
+}
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("is-show");
-  overlay.classList.add("is-show");
-});
+if(document.querySelector('.modal-map') && document.querySelector('.modal-write-us')){
+  var modalMap = document.querySelector('.modal-map');
+  var buttonMap = document.querySelector('.map-link');
+  var modalFeedback = document.querySelector('.modal-write-us');
+  var buttonFeedback = document.querySelector('.button-link');
+  var buttonCloseMap = modalMap.querySelector('.modal-close');
+  var buttonCloseFeedback = modalFeedback.querySelector('.modal-close');
 
-  close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("is-show");
-  overlay.classList.remove("is-show");
-});
+  buttonFeedback.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.add('is-show');
+    modalFeedback.classList.add('is-show');
+  });
+
+  buttonCloseFeedback.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.remove('is-show');
+    modalFeedback.classList.remove('is-show');
+  });
+
+  buttonMap.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.add('is-show');
+    modalMap.classList.add('is-show');
+  });
+
+  buttonCloseMap.addEventListener('click', function(event){
+    event.preventDefault();
+    overlay.classList.remove('is-show');
+    modalMap.classList.remove('is-show');
+  });
+
+  document.addEventListener('keydown', function(event){
+    if(event.keyCode === 27){
+      overlay.classList.remove('is-show');
+      modalMap.classList.remove('is-show');
+      modalFeedback.classList.remove('is-show');
+      modalCart.classList.remove('is-show');/*Не работает закрытие через Esc*/
+    }
+  });
+}
